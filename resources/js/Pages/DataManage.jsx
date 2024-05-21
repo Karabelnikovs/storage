7fb5d962e9765b11d01e9cb80d0d6d57afd11c72
Karabelnikovs <125544102+Karabelnikovs@users.noreply.github.com>
2024-05-21 12:15:30 +0300

    responsivness

--- resources/js/Pages/DataManage.jsx
+++ resources/js/Pages/DataManage.jsx
@@ -8,16 +8,28 @@ const DataManage = ({ products, auth }) => {
         <>
             <AuthenticatedLayout user={user}>
                 <Head title="Manage Data" />
-
-                {products.map((product) => (
-                    <div className="w-48 h-96">
-                        <UserCard
-                            name={product.name}
-                            role={product.quantity}
-                            email={product.description}
-                        />
+                <div className="py-12">
+                    <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div className="bg-white overflow-hidden shadow-2xl sm:rounded-3xl">
+                            <div className="p-6 bg-white flex justify-center items-center flex-col ">
+                                <h2 className="text-2xl font-bold mb-4">
+                                    All Products:
+                                </h2>
+                                <div className="grid sm:grid-cols-1 lg:grid-cols-4 md:grid-cols-2 gap-10  mt-6">
+                                    {products.map((product) => (
+                                        <div className="w-48 h-96">
+                                            <UserCard
+                                                name={product.name}
+                                                role={product.quantity}
+                                                email={product.description}
+                                            />
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                ))}
+                </div>
             </AuthenticatedLayout>
         </>
     );
