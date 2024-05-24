ed9bf3e4fd6c6e068d144527378ba0eb6fe01f03
Eihentss <125543962+Eihentss@users.noreply.github.com>
2024-05-24 12:26:15 +0300

    history krāsains

--- resources/js/Pages/History/Index.jsx
+++ resources/js/Pages/History/Index.jsx
@@ -5,11 +5,9 @@ import { Head } from "@inertiajs/react";
 export default function Index({ auth, history }) {
     const { user } = auth;
 
-
     useEffect(() => {
         fetchHistory();
     }, []);
-
 
     const fetchHistory = async () => {
         try {
@@ -25,23 +23,35 @@ export default function Index({ auth, history }) {
         <AuthenticatedLayout user={user}>
             <Head title="History" />
             <div className="py-12">
-                <div className="max-w-7xl my-auto mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-xl sm:rounded-3xl pl-10">
-                        <div className="p-6 py-14 bg-white border-b border-gray-200">
-                            <div>
-                                <h3 className="text-2xl font-bold mb-4">History</h3>
-                                <ul>
-                                    {history.map((entry) => (
-                                        <li key={entry.id}>
-                                            {entry.action}: {entry.description}
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                        <div className="p-6">
+                            <h3 className="text-2xl font-bold mb-4">History</h3>
+                            <ul>
+                                {history.map((entry) => (
+                                    <li key={entry.id} className={`text-lg mb-2 ${getColorClass(entry.action)}`}>
+                                        <span className="font-semibold">{entry.action}:</span> {entry.description}
+                                    </li>
+                                ))}
+                            </ul>
                         </div>
                     </div>
                 </div>
             </div>
         </AuthenticatedLayout>
     );
+}
+
+// Funkcija, kas atgriež klasi atkarībā no darbības
+const getColorClass = (action) => {
+    switch (action) {
+        case 'Product Added':
+            return 'text-green-500'; // Zaļa krāsa
+        case 'Product Updated':
+            return 'text-yellow-500'; // Dzeltens
+        case 'Product Delited':
+            return 'text-red-600'; // Sarkans
+        default:
+            return 'text-Neutral-950'; // Sarkans
+    }
 }
