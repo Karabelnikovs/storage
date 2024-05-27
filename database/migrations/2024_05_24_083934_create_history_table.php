cdda5371c65079e3f3bc5e5f440c866aa8460479
Karabelnikovs <125544102+Karabelnikovs@users.noreply.github.com>
2024-05-27 15:52:30 +0300

    migrations

--- database/migrations/2024_05_24_083934_create_history_table.php
+++ database/migrations/2024_05_24_083934_create_history_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateHistoriesTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -33,5 +33,5 @@ class CreateHistoriesTable extends Migration
     {
         Schema::dropIfExists('histories');
     }
-}
+};
 
